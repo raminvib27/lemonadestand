aead9139b51b9022dc3866a4057d7562892b6f5e
Vibu Ramineni <raminvib27@7hills.org>
2026-09-22 10:41:10 -0400

    initial commit

---
+++ LemonadeStand.java
@@ -0,0 +1,22 @@
+// LemonadeStand.java
+// Vibu Ramineni
+// This program will be the "setup" for a lemonade stand that tracks various stats related to selling lemonade
+
+public class LemonadeStand
+{
+	private double cashOnHand;
+	private double costOfLemons = 1.5; // qty 1 lemon
+	private double costOfSugar = 2;    // qty 1 cup caster sugar
+	private double costOfIce = 0.75;   // qty 1 cup ice
+	private int qtyLemons = 0;
+	private int qtySugar = 0;
+	private int qtyIce = 0;
+	private int day = 0;
+	private int temperature = 0;
+	private int weather = 0;
+	
+	public void main(String[] args)
+	{
+		System.out.println("Hello world!");
+	}
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+// Main.java
+
+public class Main
+{ public static void main(String[] args)
+	{
+		LemonadeStand l1 = new LemonadeStand();
+		l1.setup();
+	}
+}
